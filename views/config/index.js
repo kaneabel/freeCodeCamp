b5d57ece7e5c2bace97902426f5086fec3f361f9
kaneabel <tttuan0311@gmail.com>
2018-07-15 19:30:07 +0700

    Fix server running

--- views/config/index.js
+++ views/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'fly.com', // can be overwritten by process.env.HOST
+    host: 'fcc.tuan', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     https: true,
     autoOpenBrowser: false,
